df26f11f1af50727147801914e8bfdd7a2a4f7f9
Sydney Andre <s.a.andre0217@gmail.com>
2023-06-20 22:13:05 +0000

    second and third problem complete

--- stringy.js
+++ stringy.js
@@ -1,4 +1,12 @@
 function length(string) {
-    var stringLength = string.length
+    var stringLength = string.length;
     return stringLength
+}
+function toLowerCase(string) {
+    var lowerCase = string.toLowerCase();
+    return lowerCase
+}
+function toUpperCase(string) {
+    var upperCase = string.toUpperCase();
+    return upperCase
 }
