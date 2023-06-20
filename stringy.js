a29973afd3e9b61265ec2b92937208c1f2ea9ae8
Sydney Andre <s.a.andre0217@gmail.com>
2023-06-20 22:56:34 +0000

    additional problems complete

--- stringy.js
+++ stringy.js
@@ -10,3 +10,24 @@ function toUpperCase(string) {
     var upperCase = string.toUpperCase();
     return upperCase
 }
+function toDashCase(string) {
+    var dash = string.replace(" ", "-");
+    dash.toLowerCase();
+    return dash
+}
+function beginsWith(string, char) {
+    //create if statement using [] and == to compare first character
+    if (string[0] == char) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function endsWith(string, char) {
+    //create if statment using .length - 1 and === to compare last character
+    if (string(string.length - 1) === char) {
+        return true
+    } else {
+        return false
+    }
+}
