f6732a9d5712d508130b10737c0836d40dcb7df1
Sydney Andre <s.a.andre0217@gmail.com>
2023-06-22 00:44:53 +0000

    complete typeOf

--- projects/scratch-pad/day-3/type.js
+++ projects/scratch-pad/day-3/type.js
@@ -14,9 +14,11 @@
  */
 function isArray(value) {
     // YOUR CODE BELOW HERE //
-    
-    
-    
+    if (Array.isArray(value)) {
+        return true;
+    } else {
+        return false;
+    }
     
     // YOUR CODE ABOVE HERE //
 }
@@ -31,7 +33,17 @@ function isArray(value) {
  */
 function isObject(value) {
     // YOUR CODE BELOW HERE //
-    
+    if(Array.isArray(value)) {
+        return false;
+    } else if(value === null) {
+        return false;
+    } else if(value instanceof Date) {
+        return false;
+    } else if(typeof value === 'object'){
+        return true;
+    } else {
+        return false;
+    }
     
     
     
@@ -46,6 +58,17 @@ function isObject(value) {
  */
 function isCollection(value) {
     // YOUR CODE BELOW HERE //
+    if(Array.isArray(value)) {
+        return true;
+    } else if(value === null) {
+        return false;
+    } else if(value instanceof Date) {
+        return false;
+    } else if(typeof value === 'object'){
+        return true;
+    } else {
+        return false;
+    }
     
     
     
@@ -74,8 +97,15 @@ function isCollection(value) {
  */ 
 function typeOf(value) {
     // YOUR CODE BELOW HERE //
-    
-    
+    if(Array.isArray(value)) {
+        return 'array';
+    } else if(value === null) {
+        return 'null';
+    } else if(value instanceof Date) {
+        return 'date';
+    } else {
+        return typeof value;
+    }
     
     
     // YOUR CODE ABOVE HERE //
