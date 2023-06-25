55c11efb1dcf8c0124f1f95a495ffa1084ab081f
Sydney Andre <s.a.andre0217@gmail.com>
2023-06-25 18:59:02 +0000

    studies set up

---
+++ STUDIES/variables.js
@@ -0,0 +1,13 @@
+/**
+ * VARIABLES
+ * 0. introduction section (5 sentences of summary)
+ * 1. declaration and assignment (definition)
+ * 2. var, let, const (describe these variables)
+ * 3. hoisting
+ * 
+ * for each section show code examples
+ * 0.
+ * 1.
+ * 2.
+ * 3.
+ */
