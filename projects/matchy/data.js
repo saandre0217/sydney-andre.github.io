5151ef38c79b2c3af5bbd5ddeada76fce3066d88
Sydney Andre <s.a.andre0217@gmail.com>
2023-06-27 02:22:39 +0000

    stuck on step 7

---
+++ projects/matchy/data.js
@@ -0,0 +1,124 @@
+/**
+ * Part 1
+ *
+ * In this file, we're going to practice
+ * creating and accessing data structues.
+ *
+ * See the README for detailed instructions,
+ * and read every instruction carefully.
+ */
+
+//////////////////////////////////////////////////////////////////////
+// Step 1 - Object Creation //////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var animal = {};
+animal.species = 'dog';
+animal['name'] = 'Georgia';
+animal.noises = [],
+console.log(animal);
+
+
+//////////////////////////////////////////////////////////////////////
+// Step 2 - Array Creation ///////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var noises = [];
+noises[0] = 'bark';
+noises.push('ruff');
+noises.unshift('ahoo');
+noises[noises.length] = 'woof'
+console.log(noises.length);
+console.log(noises.length - 1);
+console.log(noises);
+
+//////////////////////////////////////////////////////////////////////
+// Step 3 - Combining Step 1 and 2 ///////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+animal['noises'] = noises;
+animal.noises.push('eeek');
+console.log(animal);
+
+
+/* *******************************************************************
+ * Step 4 - Review
+ *
+ * 1. What are the different ways you can access properties on objects?
+ * you can access properties in objects using the dot notation and the 
+ * bracket notation
+ * 2. What are the different ways of accessing elements on arrays?
+ * you can access elements in an array using bracket notation and the 
+ * .length method
+ * *******************************************************************
+ */
+
+/* *******************************************************************
+ * Step 5 - Take a Break!
+ *
+ * It's super important to give your brain and yourself
+ * a rest when you can! Grab a drink and have a think!
+ * For like 10 minutes, then, BACK TO WORK! :)
+ * *******************************************************************
+ */
+
+//////////////////////////////////////////////////////////////////////
+// Step 6 - A Collection of Animals //////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var animals = [];
+animals.push(animal);
+console.log(animal);
+var duck = {
+  species: 'duck',
+  name: 'Jerome',
+  noises: ['quack', 'honk', 'sneeze', 'woosh']
+}
+animals.push(duck);
+console.log(animals);
+var frog = {
+  species: 'frog',
+  name: 'Frank',
+  noises: ['ribbit', 'croak']
+}
+animals.push(frog);
+var pig = {
+  species: 'pig',
+  name: 'Pinky',
+  noises: ['oink', 'snort']
+}
+animals.push(pig);
+
+
+//////////////////////////////////////////////////////////////////////
+// Step 7 - Making Friends ///////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+/**
+ * I will use an array for the data structure of the friends list because
+ * there are no attributes associated with the friends
+ */
+var friends = [];
+function getRandom(animals){
+  var index = Math.random;
+  return index;
+}
+friends = animals[getRandom(animals)].name;
+console.log(friends);
+animals[1]['friends'] = friends;
+
+
+/**
+ * Nice work! You're done Part 1. Pat yourself on the back and
+ * move onto Part 2 in the file called "functions.js"
+ */
+
+
+
+//////////////////////////////////////////////////////////////////////
+// DON'T REMOVE THIS CODE ////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var animal, noises, animals, friends, getRandom;
+if((typeof process !== 'undefined') &&
+   (typeof process.versions.node !== 'undefined')) {
+     module.exports.animal = animal || null;
+     module.exports.noises = noises || null;
+     module.exports.animals = animals || null;
+     module.exports.friends = friends || null;
+     module.exports.getRandom = getRandom || null;
+}
