f502f9a25a34864dbdd7a33fc1e01c52c0bbbc5f
Sydney Andre <s.a.andre0217@gmail.com>
2023-07-10 00:21:22 +0000

    started datatypes studies page

---
+++ STUDIES/datatypes.js
@@ -0,0 +1,19 @@
+/**
+ * DATATYPES
+ * 0. 
+ * 1. Number: refers to a peice of data that is an integer or decimal number
+ * 2. String: refers to data encapsulated by '' or " " 
+ * 3. Boolean: true or false
+ * 4. Array: list of any data type seperated by commas and encapsulated by []
+ * 5. Object: data collected in key: value pairs called properties encapsulated by its curly braces { }. 
+ * The keys and values are paired using colons : and the pairs or properties are separated by commas. 
+ * 6. Function: a named block of code that takes in values and performs an action or test on those values.
+ * Must be defined in the scope from which you wish to call it, and can be executed by simply using the function's 
+ * name and arguments.
+ * 7. undefined: is returned when the program knows this data is present but cannot find its value
+ * 8. null: returned when a value does not exist
+ * 9. NaN: not a number and is returned when a value is expected to be a number but it is not
+ * 10. Infinity and -Infinity
+ * 11. Simple vs Complex Data Types
+ * 12. by copy vs by reference 
+ */
